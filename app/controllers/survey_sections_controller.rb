3e38fddd8a68e4a851ec35326e190cd4429a3ba1
Kevin Jay <kjayma@gmail.com>
2014-03-08 22:22:15 -0500

    Added survey_sections pop up editor
    	survey_section_controller.erb
    	survey_sections/_form.html.erb
    	survey_sections/new.html.erb
    	survey_sections/edit.html.erb

---
+++ app/controllers/survey_sections_controller.rb
@@ -0,0 +1,69 @@
+class SurveySectionsController < ApplicationController
+
+  def new
+    @title = "Add Survey Section"
+    survey=Survey.find(params[:survey_id])
+    prev_section = SurveySection.find(params[:prev_section_id])
+    @last_survey_section = survey.survey_sections.last
+    @survey_section = survey.survey_sections.build(:title=>'New Section', :display_order=>prev_section.display_order+1, :modifiable=>true)
+  end
+
+  def edit
+    @title = "Edit Survey Section"
+    @survey_section = SurveySection.find(params[:id])
+  end
+
+  def create
+    survey=Survey.find(params[:survey_section][:survey_id])
+    SurveySection.where(:survey_id=>survey.id).where("display_order >= ?",params[:survey_section][:display_order]).update_all("display_order = display_order+1")
+    @survey_section = survey.survey_sections.build(params[:survey_section])
+    @survey_section.display_order=params[:survey_section][:display_order].to_i
+    #@survey_section.questions.build(:text=>'New question',:pick=>'none',:display_order=>0,:display_type=>'default').answers.build(:text=>'string', :response_class=>'string', :display_order=>1, :template=>true)
+    if @survey_section.save
+      redirect_to :back
+    else
+      render :action => 'new', :layout=>'colorbox'
+    end
+  end
+
+  def update
+    @title = "Update Survey Section"
+    @survey_section = SurveySection.find(params[:id])
+    if @survey_section.update_attributes(params[:survey_section])
+      redirect_to :back
+    else
+      render :action => 'edit', :layout=>'colorbox'
+    end
+  end
+
+  def destroy
+    @survey_section = SurveySection.find(params[:id])
+    if !@survey_section.survey.template && @survey_section.survey.response_sets.count>0
+      render :text=>"Reponses have already been collected for this survey, therefore it cannot be modified. Please create a new survey instead."
+      return false
+    end
+    if !@survey_section.modifiable
+      render :text=>"This section cannot be removed."
+      return false
+    end
+    if !@survey_section.questions.map{|q| q.dependency_conditions}.flatten.blank?
+      render :text=>"The following questions have logic that depend on questions in this section: \n\n"+@survey_section.questions.map{|q| q.dependency_conditions.map{|d| " - "+d.dependency.question.text}}.flatten.join('\n')+"\n\nPlease delete logic before deleting this section.".html_safe
+      return
+    end
+    @survey_section.destroy
+    render :text=>""
+  end
+
+  def sort
+    survey = Survey.find(params[:survey_id])
+    satts={:id=>params[:survey_id], :survey_sections_attributes=>{}}
+    sections = params[:survey_section]
+    sections.each_with_index do |sid, index|
+      satts[:survey_sections_attributes][index.to_s]={:id => sid,:display_order => index}
+    end
+    puts satts
+    survey.update_attributes!(satts)
+    render :nothing => true
+  end
+
+end

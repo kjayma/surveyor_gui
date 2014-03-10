301b43f8ed97b0cd9a22ba5bb68118a8b7045674
Kevin Jay <kjayma@gmail.com>
2014-03-09 20:21:58 -0400

    Modified surveyforms controller
    	surveyforms_controller.rb: add several functions to respond to
    	ajax calls, including calls to insert or modify survey sections and
    	questions, and to cut and paste questions.

--- app/controllers/surveyforms_controller.rb
+++ app/controllers/surveyforms_controller.rb
@@ -27,4 +27,76 @@ class SurveyformsController < ApplicationController
     @url = "update"
   end
 
+  def replace_survey_section
+    survey_section_id = params[:survey_section_id]
+    @survey_section = SurveySection.find(survey_section_id)
+    @question_no = 0
+    render "_survey_section_fields" , :layout=> false
+  end
+
+  def insert_new_question
+    @wizard_step = params[:wizard_step]
+    question_id = params[:question_id]
+    @question = Question.find(question_id)
+    @question_no = 0
+    @surveyform = @question.survey_section.surveyform
+    render :new, :layout=>false
+  end
+
+
+  def cut_question
+    session[:cut_question]=params[:question_id]
+    @wizard_step = params[:wizard_step]
+    if q=Question.find(params[:question_id])
+      @surveyform=q.survey_section.surveyform
+      q.update_attribute(:survey_section_id,nil)
+      @question_no = 0
+      render :new, :layout=>false
+      return true
+    end
+    render :nothing=>true
+    return false
+  end
+
+  def paste_question
+    @title="Edit Survey"
+    if session[:cut_question]
+      @question = Question.find(session[:cut_question])
+      @question_no = 0
+      @wizard_step = params[:wizard_step]
+      if params[:question_id]
+        place_under_question = Question.find(params[:question_id])
+        survey_section = place_under_question.survey_section
+        survey_section_id = survey_section.id
+        survey_section.questions.where('display_order>?',place_under_question.display_order).update_all('display_order=display_order+1')
+        @question.display_order = place_under_question.display_order+1
+        @surveyform = survey_section.surveyform
+      else
+        survey_section_id = params[:survey_section_id]
+        @question.display_order = 0
+        SurveySection.find(survey_section_id).questions.update_all('display_order = display_order+1')
+        @surveyform = SurveySection.find(survey_section_id).surveyform
+      end
+      @question.survey_section_id = survey_section_id
+
+      if @question.save
+        @surveyform.reload
+        session[:cut_question]=nil
+        render :new, :layout=>false
+      else
+        render :nothing=>true
+        return false
+      end
+    end
+  end
+
+  def replace_question
+    @wizard_step = params[:wizard_step]
+    question_id = params[:question_id]
+    @question = Question.find(question_id)
+    @question_no = 0
+    render "_question_section" , :layout=> false
+  end
+
+
 end

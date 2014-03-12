05d300d8bac6787f8c01499d7c34371be6c85b25
Kevin Jay <kjayma@gmail.com>
2014-03-11 23:24:51 -0400

    modified initializer for simple_form to shorten default text

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -70,7 +70,7 @@ SimpleForm.setup do |config|
   # config.country_priority = nil
 
   # Default size for text inputs.
-  # config.default_input_size = 50
+   config.default_input_size = 25
 
   # When false, do not use translations for labels, hints or placeholders.
   # config.translate = true

7c66603245f2fd35a703ec7511a4f91d75627671
Kevin Jay <kjayma@gmail.com>
2014-03-11 22:57:10 -0400

    Added question_helper.rb

---
+++ app/helpers/question_helper.rb
@@ -0,0 +1,46 @@
+module QuestionHelper
+  def link_to_remove_fields (name, f)
+    f.hidden_field(:_destroy) + link_to_function(image_tag("delete.png",:border => 0, :margin=>'-1em'), "remove_fields(this)")
+  end
+
+  def link_to_add_fields(name, f, association, formclass="f", table=false, div=false, dirpath=nil)
+
+    fkey=':'+ f.object.class.name.underscore+'_id'
+    setid = "{"+fkey+"=>"+f.object.id.to_s+"}"
+
+    if f.object.id
+      new_object = f.object.class.reflect_on_association(association).klass.new(eval(setid))
+    else
+      new_object = f.object.class.reflect_on_association(association).klass.new
+    end
+
+    if f.object.class.reflect_on_association(association).klass.accessible_attributes.include?('display_order')
+      new_object.display_order = f.object.class.reflect_on_association(association).klass.maximum(:display_order)+1
+    end
+    new_object.class.reflect_on_all_autosave_associations.each do |ar|
+      #build a query string that checks if f.object has an existing child object. This should be due to an
+      #accepts_nested_attributes_for relationship, causing autosave. If the child does not exist, we'll create one.
+      query_string = 'new_object.'+ar.name.to_s
+      #if child does not exist and only for belongs_to relationships
+      if !eval(query_string) && ar.macro == :belongs_to
+        build_string = 'new_object.build_'+ar.name.to_s
+        #build a new empty child record
+        eval(build_string)
+      end
+    end
+
+    fields = f.simple_fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(dirpath.to_s+association.to_s.singularize + "_fields", formclass.to_sym => builder)
+    end
+    if table
+      if div
+        #organized the grouping originally by tr. Had to do it by div for survey logic, so added this.
+        link_to_function(image_tag("addicon.png",:border => 0), "add_fields3(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+      else
+        link_to_function(image_tag("addicon.png",:border => 0), "add_fields3(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+      end
+    else
+      link_to_function(image_tag("addicon.png",:border => 0), "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    end
+  end
+end

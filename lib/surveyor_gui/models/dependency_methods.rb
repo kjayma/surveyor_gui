10e7a6e55239f8e441ec58dec79255ceefe45c20
kevin <kevin@kevin-U46E.(none)>
2014-03-18 12:46:25 -0400

    Override default Surveyor validation on Dependency model
    Surveyor validates the Dependency model for the numericality of
    question_id and question_group_id.  This validation does not appear
    to take place until a corresponding Question has already been committed,
    so these id's are available.
    
    However, at some point in the past, we needed to accept_nested_attributes
    in the Question model for dependencies.  It's not clear whether this is
    still needed, but for the moment, we're keeping it. This means
    that the dependency validations are triggered when questions are saved.
    And that means the numericality_of validations on Dependency.question_id
    should not occur on new records.
    
    	dependency_methods.rb: override validation for numericality_of on
    	question_id and question_group_id.  Only do if not a new record.

--- lib/surveyor_gui/models/dependency_methods.rb
+++ lib/surveyor_gui/models/dependency_methods.rb
@@ -3,20 +3,26 @@ module SurveyorGui
     module DependencyMethods
       def self.included(base)
         # Associations
-        base.send :belongs_to, :question
-        base.send :belongs_to, :question_group
-        base.send :has_many, :dependency_conditions, :dependent => :destroy
+        base.send :attr_accessible, :dependency_conditions_attributes
+        base.send :accepts_nested_attributes_for, :dependency_conditions, :reject_if => lambda { |d| d[:operator].blank?}, :allow_destroy => true
 
-        @@validations_already_included ||= nil
-        unless @@validations_already_included
-          # Validations
-          base.send :validates_presence_of, :rule
-          # base.send :validates_format_of, :rule, :with => /^(?:and|or|\)|\(|[A-Z]|\s)+$/ #TODO properly formed parenthesis etc.
-          #base.send :validates_numericality_of, :question_id, :if => Proc.new { |d| !d.question_group_id.nil? }
-          #base.send :validates_numericality_of, :question_group_id, :if => Proc.new { |d| !d.question_id.nil? }
+#        base.class_eval {
+#        # HACK: Remove the existing validates_numericality_of block.  By default in Surveyor, it doesn't account for
+#        # question_id/question_group_id being nil when adding a new record - it never needed to.  However, Surveyor_gui
+#        # adds accepts_nested_attributes_for dependency to the question model, which triggers the dependency validations
+#        # This means we do need to account for adding new records, and the validation has to be modified.  Unfortunately,
+#        # in Rails 3.2 there is no easy way to modify an existing validation.  We have to hack it out and replace it.
+        base.class_eval do
+          _validators.reject!{ |key, _| key == :question_id }
 
-          @@validations_already_included = true
+          _validate_callbacks.reject! do |callback|
+            if callback.raw_filter.class==ActiveModel::Validations::NumericalityValidator
+              [[:question_id], [:question_group_id]].include? callback.raw_filter.attributes
+            end
+          end
         end
+        base.send :validates_numericality_of, :question_id, :if => Proc.new { |d| d.question_group_id.nil? && !d.new_record? }
+        base.send :validates_numericality_of, :question_group_id, :if => Proc.new { |d| d.question_id.nil? && !d.new_record?}
 
         # Attribute aliases
         base.send :alias_attribute, :dependent_question_id, :question_id
@@ -25,12 +31,11 @@ module SurveyorGui
   end
 end
 
-class Dependency < ActiveRecord::Base
-  include Surveyor::Models::DependencyMethods
-
-  attr_accessor
-  attr_accessible :question_id, :question_group, :rule, :dependency_conditions_attributes
-  accepts_nested_attributes_for :dependency_conditions, :reject_if => lambda { |d| d[:operator].blank?}, :allow_destroy => true
-  belongs_to :question
-  validates_format_of :rule, :with => /^(?:and|or|\)|\(|[A-Z]|[0-9]+|\s)+$/ #TODO properly formed parenthesis etc.
-end
+#class Dependency < ActiveRecord::Base
+#  include Surveyor::Models::DependencyMethods
+#
+#  attr_accessible :question_id, :question_group, :rule, :dependency_conditions_attributes
+#  accepts_nested_attributes_for :dependency_conditions, :reject_if => lambda { |d| d[:operator].blank?}, :allow_destroy => true
+#  belongs_to :question
+#  validates_format_of :rule, :with => /^(?:and|or|\)|\(|[A-Z]|[0-9]+|\s)+$/ #TODO properly formed parenthesis etc.
+#end

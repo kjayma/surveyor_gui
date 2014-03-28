9aef9233584389124dcd42122b17975f694811ad
kevin <kevin@kevin-U46E.(none)>
2014-03-28 18:21:45 -0400

    Removed complicated link_to_add_fields from question_helper.rb
    Switched to simpler ajax code.  Don't need this any longer.
    	modified:   app/helpers/question_helper.rb

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -2,46 +2,4 @@ module QuestionHelper
   def link_to_remove_fields (name, f)
     f.hidden_field(:_destroy) + link_to_function(image_tag("delete.png",:border => 0, :margin=>'-1em'), "remove_fields(this)")
   end
-
-  def link_to_add_fields(name, id, f, association, formclass="f", table=false, div=false, dirpath=nil)
-    object_name = f.object_name.include?('[') ? /(?<=\[)(.*?)(?=_attributes\])/.match(f.object_name).to_s : f.object_name
-    fkey=':'+ object_name.underscore+'_id'
-    setid = "{"+fkey+"=>"+id.to_s+"}"
-
-    object_model = eval(object_name.camelcase)
-    if id
-      new_object = object_model.reflect_on_association(association).klass.new(eval(setid))
-    else
-      new_object = object_model.reflect_on_association(association).klass.new
-    end
-
-    if object_model.reflect_on_association(association).klass.accessible_attributes.include?('display_order')
-      new_object.display_order = object_model.reflect_on_association(association).klass.maximum(:display_order)+1
-    end
-    new_object.class.reflect_on_all_autosave_associations.each do |ar|
-      #build a query string that checks if f.object has an existing child object. This should be due to an
-      #accepts_nested_attributes_for relationship, causing autosave. If the child does not exist, we'll create one.
-      query_string = 'new_object.'+ar.name.to_s
-      #if child does not exist and only for belongs_to relationships
-      if !eval(query_string) && ar.macro == :belongs_to
-        build_string = 'new_object.build_'+ar.name.to_s
-        #build a new empty child record
-        eval(build_string)
-      end
-    end
-
-    fields = f.simple_fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(dirpath.to_s+association.to_s.singularize + "_fields", formclass.to_sym => builder)
-    end
-    if table
-      if div
-        #organized the grouping originally by tr. Had to do it by div for survey logic, so added this.
-        link_to_function(image_tag("addicon.png",:border => 0), "add_fields3(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
-      else
-        link_to_function(image_tag("addicon.png",:border => 0), "add_fields3(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
-      end
-    else
-      link_to_function(image_tag("addicon.png",:border => 0), "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
-    end
-  end
 end

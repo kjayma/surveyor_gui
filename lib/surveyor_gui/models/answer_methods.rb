ab1ce036cdad13a0fb3ad1b23bbd7baacbb2047b
kevin <kevin@kevin-U46E.(none)>
2014-03-29 17:09:34 -0400

    Fixed answer sorting a second time.
    Missed an issue after sorting and saving.  Revamped approach.
    
    	app/views/questions/_answer_fields.html.erb - added a hidden
    	field for answer.id
    
    	lib/assets/javascripts/surveyor_gui/surveyor_gui.js - added a
    	method to iterate through answers and update the display order
    	to match the position in the DOM.  Run after a user sorts the
    	answers and after replacing the answers partial.
    
    	lib/surveyor_gui/models/answer_methods.rb - eliminated an
    	unecessary method to update display order.

--- lib/surveyor_gui/models/answer_methods.rb
+++ lib/surveyor_gui/models/answer_methods.rb
@@ -6,18 +6,6 @@ module SurveyorGui
         base.send :belongs_to, :question
         base.send :has_many, :responses
         base.send :attr_accessible, :text, :response_class, :display_order, :original_choice, :hide_label, :question_id, :display_type
-
-        base.send :before_save, :update_display_order
-      end
-
-      #maintain a display order for questions
-      def update_display_order
-        answer_count = self.question.answers.count
-        if answer_count > 0
-          if self.display_order == 0
-            self.display_order = self.question.answers.maximum(:display_order)
-          end
-        end
       end
 
       def split_or_hidden_text(part = nil)

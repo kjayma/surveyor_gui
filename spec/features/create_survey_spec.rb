a86d71d112f2fcad82971194aee79bd629222c2a
kevin <kevin@kevin-U46E.(none)>
2014-05-13 12:57:11 -0400

    Move methods for create_survey_spec.rb feature  into surveyforms_helper.rb 	modified:   spec/features/create_survey_spec.rb 	new file:   spec/support/surveyforms_helpers.rb

--- spec/features/create_survey_spec.rb
+++ spec/features/create_survey_spec.rb
@@ -1,38 +1,7 @@
 require 'spec_helper'
 
-def start_a_new_survey
-  visit new_surveyform_path
-  fill_in "Title", with: "How was Boston?"
-  click_button "Save Changes"
-end
-
-def first_section_title
-  find('.survey_section h2')
-end
-
-def first_question
-  find('.question span.questions')
-end
-
-def add_question
-  click_button "Add Question"
-  expect(page).to have_css('iframe')
-  within_frame 0 do
-    find('form')
-    expect(find('h1')).to have_content("Add Question")
-  end
-end
-
-def select_question_type(type)
-  within ".question_question_type" do
-    choose(type)
-  end
-end
-
-def answers
-  wait_for_ajax
-  page.all("div.answer .question_answers_text input")
-end
+#from spec/support/surveyforms_helpers.rb
+include SurveyFormsHelpers::CreateSurvey
 
 feature "User creates a new survey using a browser",  %q{
   As a user

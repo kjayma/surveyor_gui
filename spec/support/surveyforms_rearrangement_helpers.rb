ed8d70d7b7e395eb5526eb84bff760ca654253f6
kevin <kevin@kevin-U46E.(none)>
2014-05-14 22:07:17 -0400

    Start to add integration test for repositioning survey elements.
    Added methods to create a multi-section survey to the integration test helpers,
    but it is not yet rendering questions in the correct order.
    	modified:   spec/features/create_survey_spec.rb
    	modified:   spec/support/surveyforms_creation_helpers.rb
    	new file:   spec/support/surveyforms_rearrangement_helpers.rb

---
+++ spec/support/surveyforms_rearrangement_helpers.rb
@@ -0,0 +1,24 @@
+module SurveyFormsRearrangementHelpers
+  def rearrange_everything
+    move_questions
+  end
+
+  def move_sections
+  end
+
+  def move_sections_using_cut
+  end
+
+  def move_sections_using_drag
+  end
+
+  def move_questions
+    move_questions_using_cut
+  end
+
+  def move_questions_using_cut
+  end
+
+  def move_questions_using_drag
+  end
+end

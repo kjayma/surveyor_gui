f99977ddad30e10be5d5d682693271c1bb956907
Kevin Jay <kjayma@gmail.com>
2014-05-23 15:46:36 -0400

    Added test for multiple choice questions to edit.html.erb_spec.rb
    Had to fix errors in routes that work with webrick but fail with
    rspec.  Had to add survey.id to a bunch of _url and _path calls.
    	modified:   app/views/surveyforms/_form.html.erb
    
    	modified:   app/views/surveyforms/_question_fields.html.erb
    
    	modified:   app/views/surveyforms/_survey_section_fields.html.erb
    
    	modified:   spec/views/surveyforms/edit.html.erb_spec.rb

--- spec/views/surveyforms/edit.html.erb_spec.rb
+++ spec/views/surveyforms/edit.html.erb_spec.rb
@@ -10,5 +10,38 @@ end
 
 describe "surveyforms/edit.html.erb" do
   include CapybaraHelper
+  let(:surveyform){ FactoryGirl.create(:surveyform) }
+  let(:ss){ FactoryGirl.create(:survey_section, :surveyform => surveyform, :title => "Rooms", :display_order => 0)}
+  let(:question){ FactoryGirl.create(:question, :survey_section => ss) }  
+  let(:question1){ FactoryGirl.create(
+    :question, 
+    survey_section: ss, 
+    question_type: "Multiple Choice (only one answer)",
+    answers_textbox: "Standard\nDouble\nDeluxe"
+   ) }  
   
+  before do
+    surveyform.save
+    surveyform.reload
+    ss.reload
+    question.reload
+    question1.reload
+    assign(:surveyform, surveyform)
+    assign(:question_no, 0)
+  end    
+  
+  it "renders a form" do
+    render
+    expect(response).to have_selector("form")
+  end
+
+  it "shows text questions" do
+    render
+    expect(response).to have_selector("input[type='text']")
+  end
+  
+  it "shows multiple choice questions" do
+    render
+    expect(response).to have_selector("input[type='radio']")
+  end
 end

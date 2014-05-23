1229d8c63d4802d78436807b105fc318820f6b19
Kevin Jay <kjayma@gmail.com>
2014-05-23 17:06:44 -0400

    Added specs to surveyform view to test grid questions.  They are red.
    	modified:   spec/views/surveyforms/edit.html.erb_spec.rb

--- spec/views/surveyforms/edit.html.erb_spec.rb
+++ spec/views/surveyforms/edit.html.erb_spec.rb
@@ -19,6 +19,34 @@ describe "surveyforms/edit.html.erb" do
     question_type: "Multiple Choice (only one answer)",
     answers_textbox: "Standard\nDouble\nDeluxe"
    ) }  
+   
+  let(:qg){FactoryGirl.create(:question_group, text: 'Rate the meals.') }
+  let(:question2){ FactoryGirl.create(
+    :question, 
+    text: 'Breakfast',
+    survey_section: ss, 
+    question_group: qg,
+    question_type: "Multiple Choice (only one answer)",
+    answers_textbox: "Good\nBad\nUgly"
+   ) }    
+   let(:question3){ FactoryGirl.create(
+    :question, 
+    text: 'Lunch',
+    survey_section: ss, 
+    question_group: qg,
+    question_type: "Multiple Choice (only one answer)",
+    answers_textbox: "Good\nBad\nUgly"
+   ) }   
+   let(:question4){ FactoryGirl.create(
+    :question, 
+    text: 'Dinner',
+    survey_section: ss, 
+    question_group: qg,
+    question_type: "Multiple Choice (only one answer)",
+    answers_textbox: "Good\nBad\nUgly"
+   ) } 
+  let(:question5){ FactoryGirl.create(:question, :survey_section => ss, text: "What brand of ketchup do they use?") }    
+  
   
   before do
     surveyform.save
@@ -26,6 +54,10 @@ describe "surveyforms/edit.html.erb" do
     ss.reload
     question.reload
     question1.reload
+    question2.reload
+    question3.reload
+    question4.reload
+    qg.reload
     assign(:surveyform, surveyform)
     assign(:question_no, 0)
   end    
@@ -37,11 +69,23 @@ describe "surveyforms/edit.html.erb" do
 
   it "shows text questions" do
     render
-    expect(response).to have_selector("input[type='text']")
+    expect(response).to have_selector('input[type="text"]')
   end
   
   it "shows multiple choice questions" do
     render
-    expect(response).to have_selector("input[type='radio']")
+    expect(response).to have_selector("input[type='radio'][value='Standard']")
+    expect(response).to have_selector("input[type='radio'][value='Double']")
+    expect(response).to have_selector("input[type='radio'][value='Deluxe']")
+  end
+  
+  it "shows grid questions" do
+    render
+    expect(response).to match(/2\) Rate the meals\..*Good.*Bad.*Ugly.*(?<!\))\sBreakfast.*(?<!\))\sLunch.*(?<!\))\sDinner.*/)
+  end
+  
+  it "maintains correct question numbering after grid question" do
+    render
+    expect(response).to match(/"3\) What brand of ketchup do they use?"/)
   end
 end

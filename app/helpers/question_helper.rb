50d8f7e79d8c381471e762c4da784094351acc7c
Kevin Jay <kjayma@gmail.com>
2014-06-07 19:27:38 -0400

    Change question_type radio buttons on question_editor to ease changes. 	modified:   app/helpers/question_helper.rb - added 	question_type_subset method.
    	modified:   app/views/questions/_form.html.erb

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -2,12 +2,21 @@ module QuestionHelper
   def link_to_remove_fields (name, f)
     f.hidden_field(:_destroy) + link_to(image_tag("delete.png",:border => 0, :margin=>'-1em'), "#", onclick: "remove_fields(this);")
   end
-  
+
   def adjusted_text
     if @question.part_of_group?
       @question.question_group.text
     else
       @question.text
     end
+  end
+
+  def question_type_subset(args)
+    question_types = QuestionType.all.map{|t|[t.text, t.id]}[0..16].uniq
+    ordered_types = []
+    args.each do |id|
+     ordered_types << question_types.select{|t| id == t[1]}.flatten
+    end
+    ordered_types
   end
 end

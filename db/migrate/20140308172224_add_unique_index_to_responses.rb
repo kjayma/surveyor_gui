cc6fae9017b1220176fe91e7362aa535d7f93fd1
Kevin Jay <kjayma@gmail.com>
2014-08-10 14:33:38 -0400

    Remove unique index on responses
    Prevented repeaters from working

--- db/migrate/20140308172224_add_unique_index_to_responses.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueIndexToResponses < ActiveRecord::Migration
-  def change
-    add_index(:responses, [:response_set_id, :question_id, :answer_id], :name => 'response_unique_idx', :unique => true)
-  end
-end

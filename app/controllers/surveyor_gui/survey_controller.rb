27038ce30d5398f0e3c3376b705d08f7c5ba31a5
Kevin Jay <kjayma@gmail.com>
2014-08-17 12:11:43 -0400

    Move template for layouts to a namespaced path

--- app/controllers/surveyor_gui/survey_controller.rb
+++ app/controllers/surveyor_gui/survey_controller.rb
@@ -2,7 +2,7 @@ class SurveyorGui::SurveyController < ApplicationController
   def show
     @title = "User Will See this Survey"
     @survey = Survey.find(params[:survey_id])
-    user_id = defined?(current_user) ? current_user.id : 1 
+    user_id = defined?(current_user) && current_user ? current_user.id : 1 
     ResponseSet.where('survey_id = ? and test_data = ? and user_id = ?',params[:survey_id],true, user_id).each {|r| r.destroy}
     @response_set = ResponseSet.create(:survey => @survey, :user_id => user_id, :test_data => true)
     if (@survey)
